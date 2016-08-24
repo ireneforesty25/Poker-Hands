17470020d0cf02b7d3346110442d5796d2b46a1a
ireneforesty25 <irene.foresty@ymail.com>
2016-08-23 19:16:35 -0600

    Add files via upload

---
+++ Midterm_Poker.py
@@ -0,0 +1,120 @@
+from pokerdeck import*
+
+from random import shuffle, choice
+
+
+
+def deal_hand(deck):
+    shuffle(deck)
+    hand = []
+    for _ in range(5):
+        card = choice(deck)
+        deck.remove(card)
+        hand.append(card)
+    return hand 
+    
+
+
+    
+def find_pairs(ranks):
+    pairs = []
+    for card in ranks:
+        if ranks.count(card) == 2 and card not in pairs:
+            pairs.append(card)
+    return len (pairs)
+                
+                
+
+def three_of_a_kind(ranks):
+    for card in ranks:
+        if ranks.count(card) == 3:
+            return True 
+        else:
+            return False   
+        
+        
+        
+def four_of_a_kind(ranks):
+    for card in ranks:
+        if ranks.count(card) == 4:
+            return True
+        else:
+            return False  
+        
+        
+        
+def five_of_a_kind(ranks):
+    for card in ranks:
+        if ranks.count(card) == 5:
+            return True
+        else:
+            return False  
+        
+        
+    
+        
+def full_house(ranks):
+    if find_pairs(ranks) and three_of_a_kind(ranks):
+        return True
+    else:
+        return False    
+
+        
+                         
+def straight(ranks):
+    
+    for i in range(len(ranks)):
+        if ranks[i] == "J":
+            ranks[i] == 11
+        elif ranks[i] == "Q":
+            ranks[i] == 12
+        elif ranks[i] == "K":
+            ranks[i] == 13
+        elif ranks[i] == "A":
+            ranks[i] == 14
+        else:
+            ranks[i] = int(ranks[i])
+    if(int(ranks[4]) - ranks[0] == 4):
+        return True
+    else:
+        return False
+    
+def straight(ranks):
+    
+    for i in range(len(ranks)):
+        if ranks[i] == "J":
+            ranks[i] == 11
+        elif ranks[i] == "Q":
+            ranks[i] == 12
+        elif ranks[i] == "K":
+            ranks[i] == 13
+        elif ranks[i] == "A":
+            ranks[i] == 14
+        else:
+            ranks[i] = int(ranks[i])
+    if(int(ranks[0]) - ranks[4] == -4):
+        return True
+    else:
+        return False
+    
+    
+def flush():
+    
+
+
+        
+
+deck = PokerDeck()
+shuffle(deck)
+
+hand = []
+
+
+hand = deal_hand(deck) 
+
+ranks = []
+
+for card in hand:
+    ranks.append(card[0])
+    
+  
